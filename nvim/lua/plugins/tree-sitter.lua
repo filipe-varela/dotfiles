949655972dcd59f1369477058827d56099cabc30
filipe-varela <bfagdsv250@gmail.com>
2025-02-20 21:01:56 +0000

    Init commit
    From droplet

---
+++ nvim/lua/plugins/tree-sitter.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "vim",
+      "yaml",
+      "cpp",
+      "git_config",
+      "gitcommit",
+      "git_rebase",
+      "gitignore",
+      "gitattributes",
+    },
+  },
+}
