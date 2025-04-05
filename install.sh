22cd2c19f42f3e41bfcaf05d0d734d97b2e5668d
filipe-varela <bfagdsv250@gmail.com>
2025-04-05 10:18:38 +0100

    fix devpod script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # A script to run on devpod
 
 sh setup.sh
 
-type "nix-env --version" >/dev/null 2>&1 &&
-    mkdir -p "$XDG_CONFIG_HOME"/nixpkgs &&
-    ln -sTf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix &&
-    nix-env -iA nixpkgs.myPackages
+export XDG_CONFIG_HOME="$HOME"/.config
+mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
+ln -sTf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
+nix-env -iA nixpkgs.myPackages
