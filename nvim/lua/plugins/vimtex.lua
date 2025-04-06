8db937b2293263265323824ed41fcd85cdfb4378
filipe-varela <bfagdsv250@gmail.com>
2025-04-06 12:32:44 +0100

    Add latexmk compiler options

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -7,6 +7,13 @@ return {
     vim.g.vimtex_compiler_latexmk = {
       aux_dir = "./build/aux",
       out_dir = "./build/out",
+      options = {
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+        "-shell-escape",
+      },
     }
   end,
   opts = {},
