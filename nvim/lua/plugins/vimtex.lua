9505a3502e1a1b4e719c464094222eae1d3084fe
Filipe Varela <bfagdsv250@gmail.com>
2026-01-06 15:40:37 +0000

    Simplify vimtex configuration to the what I think to be the essential

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -3,16 +3,12 @@ return {
   lazy = false,
   init = function()
     -- some configs go here
-    vim.g.vimtex_view_mode = "sioyek"
+    vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_compiler_latexmk = {
       aux_dir = "./build/aux",
       out_dir = "./build/out",
       options = {
         "-verbose",
-        "-file-line-error",
-        "-synctex=1",
-        "-interaction=nonstopmode",
-        "-shell-escape",
       },
     }
   end,
