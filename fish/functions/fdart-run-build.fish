a44600563e4b1bba0632900d156190b0bc7d0fd5
filipe-varela <bfagdsv250@gmail.com>
2026-09-16 10:00:51 +0100

    Add devpod and fvm alias
    Signed-off-by: filipe-varela <bfagdsv250@gmail.com>

---
+++ fish/functions/fdart-run-build.fish
@@ -0,0 +1,4 @@
+function fdart-run-build --wraps='fvm dart run build_runner' --description 'alias fdart-run-build=fvm dart run build_runner'
+  fvm dart run build_runner $argv
+        
+end
---
+++ fish/functions/fvr.fish
@@ -0,0 +1,4 @@
+function fvr --wraps='fvm flutter run' --description 'alias fvr=fvm flutter run'
+  fvm flutter run $argv
+        
+end
